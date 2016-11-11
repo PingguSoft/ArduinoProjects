4d482024df02e44d4b6179c99ca885d30c89508a
PingguSoft <pinggusoft@gmail.com>
2016-11-11 18:51:55 +0900

    arduino kame esp8266 integration

--- ArduinoKame/ArduinoKame/ArduinoKame.ino
+++ ArduinoKame/ArduinoKame/ArduinoKame.ino
@@ -2,66 +2,175 @@
 #include <EEPROM.h>
 #include "utils.h"
 #include "minikame.h"
+#include "esp8266.h"
 
+static MiniKame         mRobot;
+static ESP8266          mESP;
+static SerialProtocol   mSerial;
 
-static MiniKame robot;
+static u32              mCurTS;
+static u32              mLastTS;
+static u32              mLastBattTS;
+static u32              mLastHeartTS;
+static u16              mCycleTime = 0;
+static u16              mErrCtr = 0;
+static u8               mLastBatt;
+static bool             mIsShutdown = false;
+static u8               mLastAuxBtn = 0;
+
+/*
+*****************************************************************************************
+* mspCallback
+*****************************************************************************************
+*/
+s8 mspCallback(u8 cmd, u8 *data, u8 size, u8 *res)
+{
+    u16 *rc;
+    u32 *ptr;
+    u16 val;
+    s8  ret = -1;
+    s16 dist;
+
+    DUMP("MSP", data, size);
+
+    switch (cmd) {
+
+        case SerialProtocol::MSP_ANALOG:
+            res[0] = mLastBatt;
+            ret = 7;
+            break;
+
+        case SerialProtocol::MSP_STATUS:
+            *((u16*)&res[0]) = mCycleTime;
+            *((u16*)&res[2]) = mErrCtr;
+            *((u32*)&res[6]) = mIsShutdown ? 0 : 1;
+            ret = 11;
+            break;
+
+        case SerialProtocol::MSP_ALTITUDE:
+            ptr = (u32*)res;
+#if 0
+            dist = mRobotAux.getDist(0);
+            if (dist > 0)
+                *ptr = dist;
+            else
+                *ptr = 0;
+#endif
+            *ptr = 0;
+            ret = 6;
+            break;
+
+        case SerialProtocol::MSP_SET_RAW_RC:
+            rc = (u16*)data;
+
+            // roll
+            val = (*rc++ - 1000);
+
+            // pitch
+            val = (*rc++ - 1000);
+
+            // yaw
+            val = (*rc++ - 1000);
+
+            // throttle
+            val = (*rc++ - 1000);
+
+            // AUX1 - AUX4
+            val = 0;
+            for (u8 i = 0; i < 4; i++) {
+                if (*rc++ > 1700)
+                    val |= BV(i);
+            }
+            if (val != mLastAuxBtn) {
+                if (val & BV(0)) {
+
+                } else {
+
+                }
+
+                mIsShutdown = val & BV(3);
+                if (mIsShutdown) {
+                }
+                mLastAuxBtn  = val;
+            }
+            break;
+    }
+
+    return ret;
+}
 
 void setup()
 {
     Serial.begin(115200);
-    robot.init();
+    mRobot.init();
+
+    mSerial.begin(SERIAL_BPS);
+    mESP.begin(&mSerial);
+    mSerial.registerCallback(mspCallback);
 }
 
 void loop()
 {
     u8  ch;
 
+    mSerial.handleMSP();
+
+    mCurTS = millis();
+
+    // every 5sec
+    if (mCurTS - mLastBattTS > 5000) {
+//        mLastBatt = mRobotAux.getBattVolt();
+        LOG(F("VOLT:%4d\n"), mLastBatt);
+        mLastBattTS = mCurTS;
+    }
+
+
     if (Serial.available()) {
         ch = Serial.read();
 
         switch(ch) {
             case 'w':
-                robot.walk(1,550);
+                mRobot.walk(1,550);
                 break;
 
             case 'a':
-                robot.turnL(1,550);
+                mRobot.turnL(1,550);
                 break;
 
             case 'd':
-                robot.turnR(1,550);
+                mRobot.turnR(1,550);
                 break;
 
             case '1':
-                robot.pushUp(2,5000);
+                mRobot.pushUp(2,5000);
                 break;
 
             case '2':
-                robot.upDown(4,250);
+                mRobot.upDown(4,250);
                 break;
 
             case '3':
-                robot.jump();
+                mRobot.jump();
                 break;
 
             case '4':
-                robot.hello();
+                mRobot.hello();
                 break;
 
             case '5':
-                robot.frontBack(4,200);
+                mRobot.frontBack(4,200);
                 break;
 
             case '6':
-                robot.dance(2,1000);
+                mRobot.dance(2,1000);
                 break;
 
             case '7':
-                robot.home();
+                mRobot.home();
                 break;
 
             case 't':
-                robot.trimming();
+                mRobot.trimming();
                 break;
         }
     }
--- ArduinoKame/ArduinoKame/minikame.cpp
+++ ArduinoKame/ArduinoKame/minikame.cpp
@@ -28,7 +28,8 @@
 
 #define SERVO_CNT	8
 
-int angToUsec(float value){
+int angToUsec(float value)
+{
     return value/180 * (MAX_PULSE_WIDTH-MIN_PULSE_WIDTH) + MIN_PULSE_WIDTH;
 }
 
@@ -47,7 +48,7 @@ void MiniKame::init()
         LOG(F("trim info available\n"));
         EEPROM.get(4, trim);
         for (u8 i = 0; i < SERVO_CNT; i++) {
-            LOG(F("servo:%d, trim:%d\n"), i + 1, trim[i]);
+            LOG(F("servo:%d, trim:%d\n"), i + 1, trim[i]);
         }
     } else {
         dwSig = 0xcafebabe;
@@ -56,13 +57,11 @@ void MiniKame::init()
         EEPROM.put(4, trim);
     }
 
-    for (u8 i=0; i<8; i++)
+    for (u8 i = 0; i < SERVO_CNT; i++){
         reverse[i] = 0;
-
-    for(u8 i=0; i<8; i++){
-         oscillator[i].start();
-         servo[i].attach(pgm_read_byte(&TBL_PINS[i]));
-     }
+        oscillator[i].start();
+        servo[i].attach(pgm_read_byte(&TBL_PINS[i]));
+    }
     zero();
 }
 
@@ -87,7 +86,7 @@ void MiniKame::trimming(void)
 
     LOG(F("TRIMMING !!!\n"));
     for (u8 i = 0; i < SERVO_CNT; i++) {
-        LOG(F("servo:%d, trim:%d\n"), i + 1, trim[i]);
+        LOG(F("servo:%d, trim:%d\n"), i + 1, trim[i]);
     }
 
     while (1) {
@@ -97,7 +96,7 @@ void MiniKame::trimming(void)
             if ('1' <= ch && ch <= '8') {
                 pos = ch - '1';
                 wiggle(pos);
-                LOG(F("servo:%d, trim:%d\n"), pos + 1, trim[pos]);
+                LOG(F("servo:%d, trim:%d\n"), pos + 1, trim[pos]);
             }
 
             switch(ch) {
@@ -105,7 +104,7 @@ void MiniKame::trimming(void)
                     if (trim[pos] < 45) {
                         trim[pos]++;
                     }
-                    LOG(F("servo:%d, trim:%d\n"), pos + 1, trim[pos]);
+                    LOG(F("servo:%d, trim:%d\n"), pos + 1, trim[pos]);
                     servo[pos].write(90 + trim[pos]);
                     break;
 
@@ -113,14 +112,14 @@ void MiniKame::trimming(void)
                     if (trim[pos] > -45) {
                         trim[pos]--;
                     }
-                    LOG(F("servo:%d, trim:%d\n"), pos + 1, trim[pos]);
+                    LOG(F("servo:%d, trim:%d\n"), pos + 1, trim[pos]);
                     servo[pos].write(90 + trim[pos]);
                     break;
 
                 case 's':
                     LOG(F("-- Saving Trim Info --\n"));
                     for (u8 i = 0; i < SERVO_CNT; i++) {
-                        LOG(F("servo:%d, trim:%d\n"), i + 1, trim[i]);
+                        LOG(F("servo:%d, trim:%d\n"), i + 1, trim[i]);
                     }
                     EEPROM.put(4, trim);
                     break;
@@ -133,7 +132,8 @@ void MiniKame::trimming(void)
     }
 }
 
-void MiniKame::turnR(float steps, float T=600){
+void MiniKame::turnR(float steps, float T=600)
+{
     int x_amp = 15;
     int z_amp = 15;
     int ap = 15;
@@ -146,7 +146,8 @@ void MiniKame::turnR(float steps, float T=600){
     execute(steps, period, amplitude, offset, phase);
 }
 
-void MiniKame::turnL(float steps, float T=600){
+void MiniKame::turnL(float steps, float T=600)
+{
     int x_amp = 15;
     int z_amp = 15;
     int ap = 15;
@@ -159,7 +160,8 @@ void MiniKame::turnL(float steps, float T=600){
     execute(steps, period, amplitude, offset, phase);
 }
 
-void MiniKame::dance(float steps, float T=600){
+void MiniKame::dance(float steps, float T=600)
+{
     int x_amp = 0;
     int z_amp = 40;
     int ap = 30;
@@ -172,7 +174,8 @@ void MiniKame::dance(float steps, float T=600){
     execute(steps, period, amplitude, offset, phase);
 }
 
-void MiniKame::frontBack(float steps, float T=600){
+void MiniKame::frontBack(float steps, float T=600)
+{
     int x_amp = 30;
     int z_amp = 25;
     int ap = 20;
@@ -185,7 +188,8 @@ void MiniKame::frontBack(float steps, float T=600){
     execute(steps, period, amplitude, offset, phase);
 }
 
-void MiniKame::run(float steps, float T=5000){
+void MiniKame::run(float steps, float T=5000)
+{
     int x_amp = 15;
     int z_amp = 15;
     int ap = 15;
@@ -207,7 +211,8 @@ void MiniKame::run(float steps, float T=5000){
     execute(steps, period, amplitude, offset, phase);
 }
 
-void MiniKame::omniWalk(float steps, float T, bool side, float turn_factor){
+void MiniKame::omniWalk(float steps, float T, bool side, float turn_factor)
+{
     int x_amp = 15;
     int z_amp = 15;
     int ap = 15;
@@ -225,24 +230,24 @@ void MiniKame::omniWalk(float steps, float T, bool side, float turn_factor){
                         90-hi
                     };
 
-    int phase[8];
-    if (side){
+    int phase[SERVO_CNT];
+    if (side) {
         int phase1[] =  {0,   0,   90,  90,  180, 180, 90,  90};
         int phase2R[] = {0,   180, 90,  90,  180, 0,   90,  90};
-        for (u8 i=0; i<8; i++)
+        for (u8 i = 0; i < SERVO_CNT; i++)
             phase[i] = phase1[i]*(1-turn_factor) + phase2R[i]*turn_factor;
-    }
-    else{
+    } else {
         int phase1[] =  {0,   0,   90,  90,  180, 180, 90,  90};
         int phase2L[] = {180, 0,   90,  90,  0,   180, 90,  90};
-        for (u8 i=0; i<8; i++)
+        for (u8 i = 0; i < SERVO_CNT; i++)
             phase[i] = phase1[i]*(1-turn_factor) + phase2L[i]*turn_factor + oscillator[i].getPhaseProgress();
     }
 
     execute(steps, period, amplitude, offset, phase);
 }
 
-void MiniKame::moonwalkL(float steps, float T=5000){
+void MiniKame::moonwalkL(float steps, float T=5000)
+{
     int z_amp = 45;
     float period[] = {T, T, T, T, T, T, T, T};
     int amplitude[] = {0,0,z_amp,z_amp,0,0,z_amp,z_amp};
@@ -252,7 +257,8 @@ void MiniKame::moonwalkL(float steps, float T=5000){
     execute(steps, period, amplitude, offset, phase);
 }
 
-void MiniKame::walk(float steps, float T=5000){
+void MiniKame::walk(float steps, float T=5000)
+{
     int x_amp = 15;
     int z_amp = 20;
     int ap = 20;
@@ -271,7 +277,7 @@ void MiniKame::walk(float steps, float T=5000){
                     };
     int  phase[] = {90, 90, 270, 90, 270, 270, 90, 270};
 
-    for (u8 i=0; i<8; i++){
+    for (u8 i = 0; i < SERVO_CNT; i++) {
         oscillator[i].reset();
         oscillator[i].setPeriod(period[i]);
         oscillator[i].setAmplitude(amplitude[i]);
@@ -301,7 +307,8 @@ void MiniKame::walk(float steps, float T=5000){
     }
 }
 
-void MiniKame::upDown(float steps, float T=5000){
+void MiniKame::upDown(float steps, float T=5000)
+{
     int x_amp = 0;
     int z_amp = 35;
     int ap = 20;
@@ -324,7 +331,8 @@ void MiniKame::upDown(float steps, float T=5000){
 }
 
 
-void MiniKame::pushUp(float steps, float T=600){
+void MiniKame::pushUp(float steps, float T=600)
+{
     int z_amp = 40;
     int x_amp = 65;
     int hi = 30;
@@ -336,7 +344,8 @@ void MiniKame::pushUp(float steps, float T=600){
     execute(steps, period, amplitude, offset, phase);
 }
 
-void MiniKame::hello(){
+void MiniKame::hello()
+{
     float sentado[]={90+15,90-15,90-65,90+65,90+20,90-20,90+10,90-10};
     moveServos(150, sentado);
     delay(200);
@@ -359,7 +368,8 @@ void MiniKame::hello(){
 
 
 
-void MiniKame::jump(){
+void MiniKame::jump()
+{
     float sentado[]={90+15,90-15,90-65,90+65,90+20,90-20,90+10,90-10};
     int ap = 20;
     int hi = 35;
@@ -371,18 +381,22 @@ void MiniKame::jump(){
     home();
 }
 
-void MiniKame::home(){
+void MiniKame::home()
+{
     int ap = 20;
     int hi = 35;
     int position[] = {90+ap,90-ap,90-hi,90+hi,90-ap,90+ap,90+hi,90-hi};
     for (u8 i=0; i<8; i++) setServo(i, position[i]);
 }
 
-void MiniKame::zero(){
-    for (u8 i=0; i<8; i++) setServo(i, 90);
+void MiniKame::zero()
+{
+    for (u8 i = 0; i < SERVO_CNT; i++)
+        setServo(i, 90);
 }
 
-void MiniKame::reverseServo(int id){
+void MiniKame::reverseServo(int id)
+{
     if (reverse[id])
         reverse[id] = 0;
     else
@@ -390,7 +404,8 @@ void MiniKame::reverseServo(int id){
 }
 
 
-void MiniKame::setServo(int id, float target){
+void MiniKame::setServo(int id, float target)
+{
     if (!reverse[id])
         servo[id].writeMicroseconds(angToUsec(target+trim[id]));
     else
@@ -398,31 +413,36 @@ void MiniKame::setServo(int id, float target){
     _servo_position[id] = target;
 }
 
-float MiniKame::getServo(int id){
+float MiniKame::getServo(int id)
+{
     return _servo_position[id];
 }
 
 
-void MiniKame::moveServos(int time, float target[8]) {
-    if (time>10){
-        for (u8 i = 0; i < 8; i++)	_increment[i] = (target[i] - _servo_position[i]) / (time / 10.0);
+void MiniKame::moveServos(int time, float target[8])
+{
+    if (time > 10) {
+        for (u8 i = 0; i < SERVO_CNT; i++)
+            _increment[i] = (target[i] - _servo_position[i]) / (time / 10.0);
         _final_time =  millis() + time;
 
-        while (millis() < _final_time){
+        while (millis() < _final_time) {
             _partial_time = millis() + 10;
-            for (u8 i = 0; i < 8; i++) setServo(i, _servo_position[i] + _increment[i]);
+            for (u8 i = 0; i < SERVO_CNT; i++)
+                setServo(i, _servo_position[i] + _increment[i]);
             while (millis() < _partial_time); //pause
         }
+    } else {
+        for (u8 i = 0; i < SERVO_CNT; i++)
+            setServo(i, target[i]);
     }
-    else{
-        for (u8 i = 0; i < 8; i++) setServo(i, target[i]);
-    }
-    for (u8 i = 0; i < 8; i++) _servo_position[i] = target[i];
+    for (u8 i = 0; i < SERVO_CNT; i++)
+        _servo_position[i] = target[i];
 }
 
-void MiniKame::execute(float steps, float period[8], int amplitude[8], int offset[8], int phase[8]){
-
-    for (u8 i=0; i<8; i++){
+void MiniKame::execute(float steps, float period[8], int amplitude[8], int offset[8], int phase[8])
+{
+    for (u8 i = 0; i < SERVO_CNT; i++) {
         oscillator[i].setPeriod(period[i]);
         oscillator[i].setAmplitude(amplitude[i]);
         oscillator[i].setPhase(phase[i]);
@@ -431,12 +451,16 @@ void MiniKame::execute(float steps, float period[8], int amplitude[8], int offse
 
     unsigned long global_time = millis();
 
-    for (u8 i=0; i<8; i++) oscillator[i].setTime(global_time);
+    for (u8 i = 0; i < SERVO_CNT; i++)
+        oscillator[i].setTime(global_time);
 
     _final_time = millis() + period[0]*steps;
     while (millis() < _final_time){
-        for (u8 i=0; i<8; i++){
-            setServo(i, oscillator[i].refresh());
+        for (u8 i = 0; i < SERVO_CNT; i++) {
+            float angle = oscillator[i].refresh();
+            if (i == 0)
+                LOG(F("angle : %d\n"), (int)angle);
+            setServo(i, angle);
         }
     }
 }
